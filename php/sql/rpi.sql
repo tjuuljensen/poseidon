8a7872cc3402b5a787ad6ab7b20e2b0be2c5102f
Torsten Juul-Jensen <tjuuljensen@gmail.com>
2013-11-25 12:45:04 +0100

    Libraries in /var/www/php renamed

--- php/sql/rpi.sql
+++
@@ -1,7 +0,0 @@
-/*
--- Query: SELECT * FROM poseidondb.rpi
-LIMIT 0, 1000
-
--- Date: 2013-05-05 23:35
-*/
-INSERT INTO `rpi` (`id`,`mac`,`hostip`,`publicip`) VALUES (1,' b8:27:eb:dc:46:6b','192.168.1.115','93.163.139.218');
